b3b41fe224f21b3973a67aaafb0828557dc15ada
Yerkulan <kalidolda.ye@chocofood.kz>
2022-08-05 00:01:49 +0600

    added order save query and thanks page

--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,6 @@ from crm import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.first_page)
+    path('', views.first_page),
+    path('thanks/', views.thanks_page, name='thanks')
 ]
--- crm/views.py
+++ crm/views.py
@@ -6,3 +6,17 @@ from .models import Order
 def first_page(request):
     object_list = Order.objects.all()
     return render(request, './index.html', {'object_list': object_list})
+
+
+def thanks_page(request):
+    name = request.GET['name']
+    phone = request.GET['phone']
+    order = Order(order_name=name, order_phone=phone)
+    order.save()
+    return render(
+        request, './thanks_page.html',
+        {
+            'name': name,
+            'phone': phone
+        }
+    )
