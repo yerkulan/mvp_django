e0752103dc3a5530365db6b3e73742f5f9472489
Yerkulan <kalidolda.ye@chocofood.kz>
2022-08-04 22:15:22 +0600

    changed directory name to app

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for landingpage_django project.
+WSGI config for app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landingpage_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
