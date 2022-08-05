596dd651aa84ecbcfff3bed914ba81f957f81c1c
Yerkulan <kalidolda.ye@chocofood.kz>
2022-08-05 14:14:05 +0600

    base.html, static and media root

--- crm/views.py
+++ crm/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForm
 
 
 # Create your views here.
 def first_page(request):
     object_list = Order.objects.all()
-    return render(request, './index.html', {'object_list': object_list})
+    form = OrderForm()
+    return render(
+        request, './index.html', {
+            'object_list': object_list,
+            'form': form,
+        }
+    )
 
 
 def thanks_page(request):
